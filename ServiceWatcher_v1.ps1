28fbea4d16a6f4539a40421d2102e4275bfbbbb4
JoshuaSHenderson <60445409+JoshuaSHenderson@users.noreply.github.com>
2021-12-03 11:37:06 -0500

    Update ServiceWatcher_v1.ps1
    Added some documentation

--- ServiceWatcher_v1.ps1
+++ ServiceWatcher_v1.ps1
@@ -5,12 +5,19 @@ Function Watch-ServiceStatus {
 
     #create default email variables
     $User = "jhxcoal@gmail.com"
-    $File = "C:\Scripts\ServiceWatcher\EmailPassword.txt"
+    
+    #YOU NEED TO CREATE A FILE WITH THE PASSWORD. ENSURE THIS FILE IS ENCRYPTED BY USING "myPassword" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File "C:\EmailPassword.txt"#YOU NEED TO CREATE A FILE WITH THE PASSWORD. ENSURE THIS FILE IS ENCRYPTED BY USING "myPassword" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File "C:\EmailPassword.txt"
+    #PLACE THIS FILE SOMEWHERE THE SCRIPT CAN GET TO IT. IN THIS EXAMPLE, ALL FILES FOR THE SCRIPT ARE LOCATED IN C:\Scripts\ServiceWatcher\
+    $File = "C:\Scripts\ServiceWatcher\EmailPassword.txt" 
+    
     $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (Get-Content $File | ConvertTo-SecureString)
-    $EmailTo = "josh.henderson@xcoal.com"
+    $EmailTo = "josh.henderson@xcoal.com" #WHO WILL RECEIVE THE EMAIL
+    
+    #If multiple emails are needed, add additional emails here
     $EmailTo2 = "Chris.Lynch@xcoal.com"
-    $EmailFrom = "jhxcoal@gmail.com"
-    $SMTPServer = "smtp.gmail.com" 
+   
+    $EmailFrom = "jhxcoal@gmail.com" #YOUR EMAIL HERE
+    $SMTPServer = "smtp.gmail.com"  #THIS EXAMPLE USSES GMAIL
     $SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
     $SMTPMessage2 = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo2,$Subject,$Body)
     $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
@@ -29,14 +36,19 @@ Function Watch-ServiceStatus {
         $SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
 	$SMTPMessage2 = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo2,$Subject,$Body)
         $SMTPClient.Send($SMTPMessage)
-	$SMTPClient.Send($SMTPMessage2)
+	
+	#Uncomment the below if you need to send multiple emails
+	#$SMTPClient.Send($SMTPMessage2)
         }
-
+	
+	#Uncomment below to start the service after sending the email
         #Start-Service $Name
 
     }
 
 }
 
+#Add a new line for each service needed to be monitored. Set -Notify to $true to send out the email. Note: the display name and the Servic name may be different be sure to check the service properties for the correct name
+#Phone Service is used as an example. This service should already not be running so it works well for test
 Watch-ServiceStatus -Name 'Phone Service' -Notify $true
-Watch-ServiceStatus -Name 'RightAngle Service Monitor (20.0)' -Notify $true
+#Watch-ServiceStatus -Name 'REPLACE WITH YOUR SERVICE' -Notify $true
